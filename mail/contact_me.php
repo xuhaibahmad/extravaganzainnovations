e0a75b072d63b70b92f95700742bb43674279e0e
Zuhaib Ahmad <xuhaib_ahmad@yahoo.com>
2017-03-18 12:52:16 +0500

    Revert "Update email to temporarily available one"
    This reverts commit 1db830ec92e074d962d415dd1fb49c9c4b3d1e1d.

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -16,7 +16,7 @@ $phone = $_POST['phone'];
 $message = $_POST['message'];
 
 // Create the email and send the message
-$to = 'contact@zuhaibahmad.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'mail@extravaganzainnovations.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Quote Request:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
 $headers = "From: info@extravaganzainnovations.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
